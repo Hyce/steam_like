42e57ae1bf907b88e0701542d4b67af17e11e2ad
Hyce <jon.poullain@gmail.com>
2017-01-18 11:37:40 +0100

    profil, avancement html

--- index.php
+++ index.php
@@ -40,8 +40,8 @@ include 'header.php';
     <!-- Portfolio Item Heading -->
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Portfolio Item
-                <small>Item Subheading</small>
+            <h1 class="page-header">Profil
+                <small>(Pseudo)</small>
             </h1>
         </div>
     </div>
@@ -50,19 +50,19 @@ include 'header.php';
     <!-- Portfolio Item Row -->
     <div class="row">
 
-        <div class="col-md-8">
-            <img class="img-responsive" src="http://placehold.it/750x500" alt="">
+        <div class="col-md-4">
+            <img class="img-responsive" src="http://placehold.it/300x300" alt="">
         </div>
 
-        <div class="col-md-4">
-            <h3>Project Description</h3>
+        <div class="col-md-8">
+            <h3>Description</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-            <h3>Project Details</h3>
-            <ul>
-                <li>Lorem Ipsum</li>
-                <li>Dolor Sit Amet</li>
-                <li>Consectetur</li>
-                <li>Adipiscing Elit</li>
+            <h3>Infos</h3>
+            <ul style="list-style: none;">
+                <li>Nom :</li>
+                <li>Prénom :</li>
+                <li>E-mail :</li>
+                <li>date de Naissance :</li>
             </ul>
         </div>
 
@@ -73,7 +73,7 @@ include 'header.php';
     <div class="row">
 
         <div class="col-lg-12">
-            <h3 class="page-header">Related Projects</h3>
+            <h3 class="page-header">Jeux Possédés</h3>
         </div>
 
         <div class="col-sm-3 col-xs-6">
