37c83b7d64b41321f3568a3d6d4ecdced7280240
Unknown <joshua@tabetozor.com>
2017-01-19 22:56:09 +0100

    add menu.php and store.php

--- menu.php
+++ menu.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Joshua
- * Date: 19/01/2017
- * Time: 22:49
- */
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Steamy</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav" style="float: right;">
+                <li>
+                    <a href="index.php">Acceuil</a>
+                </li>
+                <li>
+                    <a href="store.php">Store</a>
+                </li>
+                <li>
+                    <a href="#">Panier</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
